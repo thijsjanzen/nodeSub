2cb617881fdb881c9ba39928b54b077f5330adc7
Thijs Janzen <thijsjanzen@gmail.com>
2019-04-11 15:14:53 +0200

    improve test coverage

--- tests/testthat/test-getcoverage.R
+++ tests/testthat/test-getcoverage.R
@@ -5,6 +5,10 @@ test_that("sim_dual_independent", {
 
   sequences <- sim_dual_independent(phy)
   testthat::expect_true(class(sequences) == "phyDat")
+
+  Q_JC <- matrix(1, nrow = 4, ncol = 4)
+  sequences <- sim_dual_independent(phy, Q = Q_JC)
+  testthat::expect_true(class(sequences) == "phyDat")
 })
 
 test_that("sim_dual_parent", {
@@ -12,12 +16,22 @@ test_that("sim_dual_parent", {
 
   sequences <- sim_dual_parent(phy)
   testthat::expect_true(class(sequences) == "phyDat")
+
+  Q_JC <- matrix(1, nrow = 4, ncol = 4)
+  sequences <- sim_dual_parent(phy, Q = Q_JC)
+  testthat::expect_true(class(sequences) == "phyDat")
+
 })
 
 test_that("sim_dual_linked", {
   phy  <- phytools::read.newick(text = "(t1:10,(t3:2,t2:2):8);")
 
   sequences <- sim_dual_linked(phy)
+  testthat::expect_true(class(sequences) == "phyDat")
+
+  Q_JC <- matrix(1, nrow = 4, ncol = 4)
+
+  sequences <- sim_dual_linked(phy, Q = Q_JC)
   testthat::expect_true(class(sequences) == "phyDat")
 })
 
--- tests/testthat/test-sim_normal.R
+++ tests/testthat/test-sim_normal.R
@@ -2,11 +2,27 @@ context("nodeSub_sim_normal")
 
 test_that("sim_normal", {
   phy  <- phytools::read.newick(text = "(t1:10,(t3:2,t2:2):8);")
-
-  seq_phangorn <- phangorn::simSeq(x = phy, l = 10000, rate = 0.1)
+  set.seed(43) # for reproducability
+  seq_phangorn <- phangorn::simSeq(x = phy, l = 10000, rate = 0.01)
   dist_phangorn <- phangorn::dist.ml(seq_phangorn)
 
-  seq_nodeSub <- sim_normal(x = phy, l = 10000,  rate = 0.1)
+  seq_nodeSub <- sim_normal(x = phy, l = 10000,  rate = 0.01)
+  dist_nodeSub <- phangorn::dist.ml(seq_nodeSub)
+
+  testthat::expect_equal(dist_phangorn[1], dist_nodeSub[1], tolerance = 0.05)
+  testthat::expect_equal(dist_phangorn[2], dist_nodeSub[2], tolerance = 0.05)
+  testthat::expect_equal(dist_phangorn[3], dist_nodeSub[3], tolerance = 0.05)
+})
+
+test_that("JC", {
+  phy  <- phytools::read.newick(text = "(t1:10,(t3:2,t2:2):8);")
+
+  Q_JC <- matrix(1, nrow = 4, ncol = 4)
+  set.seed(42) # for reproducability
+  seq_phangorn <- phangorn::simSeq(x = phy, Q = Q_JC, l = 10000, rate = 0.01)
+  dist_phangorn <- phangorn::dist.ml(seq_phangorn)
+
+  seq_nodeSub <- sim_normal(x = phy, Q = Q_JC, l = 10000,  rate = 0.01)
   dist_nodeSub <- phangorn::dist.ml(seq_nodeSub)
 
   testthat::expect_equal(dist_phangorn[1], dist_nodeSub[1], tolerance = 0.05)
