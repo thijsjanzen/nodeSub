91dbd7e472e65d47b44e26cd5e3d4506289770be
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-14 12:29:13 +0200

    'sim_dual_(independent|linked)' give error message when the DNA sequence length 'l' mismatches the length of the root sequence 'rootseq'. Fix #5

--- tests/testthat/test-sim_dual_independent.R
+++ tests/testthat/test-sim_dual_independent.R
@@ -1,3 +1,13 @@
-test_that("use", {
-  expect_equal(2 * 2, 4)
+test_that("abuse", {
+
+  root_sequence <- "acgt"
+
+  expect_error(
+      sim_dual_independent(
+        phy = ape::rcoal(3),
+        rootseq = strsplit(root_sequence, split = "")[[1]],
+        l = 1234500
+      ),
+    "'rootseq' must have the same length as 'l'"
+  )
 })
--- tests/testthat/test-sim_dual_linked.R
+++ tests/testthat/test-sim_dual_linked.R
@@ -1,3 +1,14 @@
-test_that("use", {
-  expect_equal(2 * 2, 4)
+test_that("abuse", {
+
+  root_sequence <- "acgt"
+
+  expect_error(
+    sim_dual_linked(
+      phy = ape::rcoal(3),
+      rootseq = strsplit(root_sequence, split = "")[[1]],
+      l = 12345
+    ),
+    "'rootseq' must have the same length as 'l'"
+  )
+
 })
