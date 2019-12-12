f0865436f2c09f9117d84bcfeea41429ccd280b1
Thijs Janzen <thijsjanzen@gmail.com>
2019-12-12 14:11:47 +0100

    Update test-relative_node_time.R

--- tests/testthat/test-relative_node_time.R
+++ tests/testthat/test-relative_node_time.R
@@ -7,47 +7,20 @@ test_that("required_node_time use", {
                                      lambda = 1,
                                      mu = 0,
                                      complete = FALSE)[[1]]
-  node_time <- 0.5
-  predicted_frac <- calc_expected_fraction(focal_tree,
-                                           node_time = node_time,
-                                           model = "linked",
-                                           lambda = 1,
-                                           mu = 0)
 
-  found <- c()
-  sub_rate <- 0.01
-  for (repl in 1:10) {
-    vx <- sim_dual_linked(focal_tree,
-                          rate = sub_rate,
-                          node_mut_rate_double = sub_rate,
-                          l = 1000,
-                          node_time = node_time)
-    found[repl] <- vx$total_node_substitutions /
-      (vx$total_node_substitutions + vx$total_branch_substitutions)
+  chosen_fraction <- 0.1
+  req_node_time <- nodeSub::calc_required_node_time(focal_tree,
+                                                    s = chosen_fraction)
 
-  }
+  obs_fraction <- nodeSub::calc_fraction(focal_tree, node_time = req_node_time)
 
-  testthat::expect_equal(predicted_frac, mean(found), tolerance = 0.05)
+  testthat::expect_equal(obs_fraction, 0.1)
 
-  node_time <- 0.5
-  predicted_frac <- calc_expected_fraction(focal_tree,
-                                           node_time = node_time,
-                                           model = "unlinked",
-                                           lambda = 1,
-                                           mu = 0)
+  ali <- nodeSub::sim_dual_independent(focal_tree, rate1 = 0.001, rate2 = 0.001,
+                                       l = 10000, node_time = req_node_time)
 
-  found <- c()
-  sub_rate <- 0.01
-  for (repl in 1:10) {
-    vx <- sim_dual_independent(focal_tree,
-                               rate1 = sub_rate,
-                               rate2 = sub_rate,
-                               l = 1000,
-                               node_time = node_time)
-    found[repl] <- vx$total_node_substitutions /
-                  (vx$total_node_substitutions + vx$total_branch_substitutions)
+  obs_fraction <- ali$total_node_substitutions / (ali$total_node_substitutions + ali$total_branch_substitutions)
 
-  }
+  testthat::expect_equal(obs_fraction, chosen_fraction, tol = 0.01)
 
-  testthat::expect_equal(predicted_frac, mean(found), tolerance = 0.05)
 })
