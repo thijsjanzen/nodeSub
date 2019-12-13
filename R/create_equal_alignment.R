49eb57e827ef21feb21388673b52dfefb54e7e9b
Thijs Janzen <thijsjanzen@gmail.com>
2019-12-13 12:37:36 +0100

    remove numeric optimization of mutation rate

--- R/create_equal_alignment.R
+++ R/create_equal_alignment.R
@@ -25,12 +25,12 @@ create_equal_alignment <- function(input_tree,
   num_hidden_nodes <- count_hidden(input_tree)
 
   factor <- 1
-  if(node_model == "unlinked") factor <- 2
+  if (node_model == "unlinked") factor <- 2
 
-  H <- (factor * num_nodes + num_hidden_nodes) * node_time
+  total_on_nodes <- (factor * num_nodes + num_hidden_nodes) * node_time
   total_bl <- sum(geiger::drop.extinct(input_tree)$edge.length)
 
-  adjusted_rate <-  expected_rate2 <- sub_rate + sub_rate * H / total_bl
+  adjusted_rate <-  sub_rate + sub_rate * total_on_nodes / total_bl
 
   proposed_alignment <- alt_model(input_tree,
                                   adjusted_rate,
@@ -58,17 +58,12 @@ create_equal_alignment <- function(input_tree,
     alignments <- lapply(alignments, propose_alignments, adjusted_rate)
     all_subs <- unlist(lapply(alignments, calc_subs))
 
-    num_matches = length(which(all_subs == num_emp_subs))
+    num_matches <- length(which(all_subs == num_emp_subs))
 
     if (num_matches > 0) {
       a <- which(all_subs == num_emp_subs)[[1]]
       return(list("alignment" = alignments[[a]],
                   "rate" = adjusted_rate))
-    } else {
-      avg_sub <- mean(all_subs, na.rm = TRUE)
-      factor <- num_emp_subs / avg_sub
-      stored_factor <- factor
-      adjusted_rate <- adjusted_rate * factor
     }
     cnt <- cnt + length(all_subs)
     if (verbose) cat(cnt, adjusted_rate, mean(all_subs, na.rm = TRUE),
