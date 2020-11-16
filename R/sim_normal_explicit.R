932087b08fb2edc33c7d0c6fb71d2742da05be68
Thijs Janzen <thijsjanzen@gmail.com>
2020-11-16 20:02:30 +0100

    add explicit functions

---
+++ R/sim_normal_explicit.R
@@ -0,0 +1,164 @@
+#' @keywords internal
+ab_n <- function(ab_nmin1, n) {
+  if(n == 0) {
+    ab_n <- c(1, 0)
+  } else {
+    ab_n <- c( 3 * ab_nmin1[2] / n, (2 * ab_nmin1[2] + ab_nmin1[1]) / n )
+  }
+  return(ab_n)
+}
+
+#' @keywords internal
+ab <- function(N) {
+  ab_mat <- matrix(0,
+                   nrow = N + 1,
+                   ncol = 2)
+  ab_mat[1,] <- c(1, 0)
+  for(i in 2:(N + 1)) {
+    ab_mat[i, ] <- ab_n(ab_mat[i - 1, ], i - 1)
+  }
+  return(ab_mat)
+}
+
+#' @keywords internal
+P_n <- function(N, mu, t) {
+  ab_mat <- ab(N)
+  P <- ab_mat * (mu * t) ^ (0:N) * exp(-3 * mu * t)
+  return(P)
+}
+
+#' @keywords internal
+mutate_base <- function(base, Pn) {
+  bases <- c("a", "c", "g", "t")
+  index <- sample(x = 1:2, size = 1, prob = colSums(Pn))
+  if (index == 1) {
+    num_mut <- sample(x = 0:(-1 + length(Pn[, 1])), size = 1, prob = Pn[, 1])
+    return(c(base, num_mut))
+  }
+  if (index == 2) {
+    num_mut <- sample(x = 0:(-1 + length(Pn[, 2])), size = 1, prob = Pn[, 2])
+    other_bases <- bases[-which(bases == base)]
+    chosen_base <- sample(other_bases, size = 1)
+    return(c(chosen_base, num_mut))
+  }
+}
+
+
+
+#' Simulate sequences.
+#'
+#' Simulate sequences for a given evolutionary tree.
+#'
+#' \code{simSeq} is now a generic function to simulate sequence alignments to
+#' along a phylogeny. It
+#' is quite flexible and allows to generate DNA, RNA, amino acids, codon  or
+#' binary sequences.  It is possible to give a \code{pml} object as input simSeq
+#' return a \code{phyDat} from these model.  There is also a more low level
+#' version, which lacks rate variation, but one can combine different
+#' alignments having their own rate (see example). The rate parameter acts like
+#' a scaler for the edge lengths.
+#'
+#' can be supplied.
+#'
+#' @param x a phylogenetic tree \code{tree}, i.e. an object of class
+#' \code{phylo} or and object of class \code{pml}.
+#' @param l length of the sequence to simulate.
+#' @param Q the rate matrix.
+#' @param bf base frequencies.
+#' @param rootseq a vector of length l containing the root sequence, other root
+#' sequence is randomly generated.
+#' @param rate mutation rate or scaler for the edge length, a numerical value
+#' greater than zero.
+#' @return \code{simSeq} returns an object of class phyDat.
+#' @author Klaus Schliep \email{klaus.schliep@@gmail.com}
+#' @keywords cluster
+#' @export
+sim_normal_explicit <- function(x,
+                                l = 1000,
+                                Q = NULL,   # nolint
+                                bf = NULL,
+                                rootseq = NULL,
+                                rate = 1) {
+
+
+  levels <- c("a", "c", "g", "t")
+
+  lbf <- length(levels)
+
+  if (is.null(bf)) bf <- rep(1 / lbf, lbf)
+  if (is.null(Q)) {
+    Q <- rep(1, lbf * (lbf - 1) / 2) # nolint
+  }
+  if (is.matrix(Q)) Q <- Q[lower.tri(Q)]  # nolint
+  # capital Q is retained to conform to mathematical notation on wikipedia
+  # and in the literature
+
+  eig <- phangorn::edQt(Q, bf)
+
+  m <- length(levels)
+
+  if (is.null(rootseq)) rootseq <- sample(levels, l, replace = TRUE, prob = bf)
+  x <- stats::reorder(x)
+  edge <- x$edge
+  num_nodes <- max(edge)
+  res <- matrix(NA, l, num_nodes)
+  parent <- as.integer(edge[, 1])
+  child <- as.integer(edge[, 2])
+  root <- as.integer(parent[!match(parent, child, 0)][1])
+  res[, root] <- rootseq
+  tl <- x$edge.length
+
+  total_branch_subs <- 0
+  daughter_subs <- rep(0, length(parent))
+  daughter_subs2 <- rep(0, length(parent))
+
+  utils::pb <- txtProgressBar(max = length(tl), style = 3)
+  for (i in seq_along(tl)) {
+    from <- parent[i]
+    to <- child[i]
+    P <-  P_n(100, rate, tl[i])  # get_p_matrix(tl[i], eig, rate)  # nolint
+    # capital P is retained to conform to mathematical notation on wikipedia
+    # and in the literature
+
+    seq_before_mut <- res[, from]
+    seq_after_mut <- t(sapply(seq_before_mut, mutate_base, P))
+    num_mut <- sum(as.numeric(seq_after_mut[, 2]))
+    res[, to] <- seq_after_mut[, 1]
+
+    branch_subs <- sum(res[, from] != res[, to])
+    total_branch_subs <- total_branch_subs + branch_subs
+  #  daughter_subs[i] <- branch_subs
+    daughter_subs2[i] <- num_mut
+    utils::setTxtProgressBar(pb, i)
+  }
+
+  # now, given the daughter subs string, we need to calculate the total
+  # accumulated divergence
+  updated_subs <- calc_accumulated_substitutions(x, daughter_subs2)
+ # reverse_subs <- calc_accumulated_substitutions(x, daughter_subs2)
+
+  phy_no_extinct <- geiger::drop.extinct(x)
+
+  k <- length(x$tip.label)
+  label <- c(x$tip.label, as.character((k + 1):num_nodes))
+  colnames(res) <- label
+  res <- res[, phy_no_extinct$tip.label, drop = FALSE]
+  alignment_phydat <- phyDat.DNA(as.data.frame(res, stringsAsFactors = FALSE))
+
+  total_inferred_substitutions <- sum(calc_dist(alignment_phydat, rootseq))
+
+  output <- list("alignment" = alignment_phydat,
+                 "root_seq" = rootseq,
+                 "total_branch_substitutions" = updated_subs$total_branch_subs,
+                 "total_node_substitutions" = updated_subs$total_node_subs,
+                 "total_inferred_substitutions" = total_inferred_substitutions,
+                 "total_accumulated_substitutions" =
+                   updated_subs$total_accumulated_substitutions)
+                 #"incl_reverse_subs" = reverse_subs$total_accumulated_substitutions)
+
+  return(output)
+}
+
+
+
+
---
+++ R/sim_unlinked_explicit.R
@@ -0,0 +1,121 @@
+#' simulate a sequence assuming node substitutions
+#' are shared among the offspring
+#' @param phy tree for which to simulate sequences
+#' @param Q1 substitution matrix along the branches, default = JC
+#' @param Q2 substitution matrix on the nodes, default = JC
+#' @param rate1 mutation rate along the branch, default = 0.1
+#' @param rate2 mutation rate on the node, default = 0.1
+#' @param l number of base pairs to simulate
+#' @param bf base frequencies, default = c(0.25, 0.25, 0.25, 0.25)
+#' @param rootseq sequence at the root, simulated by default
+#' @param node_time amount of time spent at the nodes
+#' @return list with four items \enumerate{
+#' \item{alignment} Phydat object with the resulting alignment
+#' \item{rootseq} the rootsequence used
+#' \item{total_branch_substitutions} total number of substitutions accumulated
+#' on the branches
+#' \item{total_node_substitutions} total number of substitutions accumulated at
+#' the nodes}
+#' @export
+sim_unlinked_explicit <- function(phy,
+                         Q1 = rep(1, 6),   # nolint
+                         Q2 = rep(1, 6),   # nolint
+                         rate1 = 0.1,
+                         rate2 = 0.1,
+                         l = 1000,
+                         bf = rep(0.25, 4),
+                         rootseq = NULL,
+                         node_time = 1e-3) {
+
+  levels <- c("a", "c", "g", "t")
+  if (is.null(rootseq)) {
+    rootseq <- sample(levels, l, replace = TRUE, prob = bf)
+  }
+  if (length(rootseq) != l) {
+    stop(
+      "'rootseq' must have the same length as 'l'. \n",
+      "length 'rootseq': ", length(rootseq), " \n",
+      "value of 'l': ", l, " \n"
+    )
+  }
+
+  # only extract the 6 important rates.
+  if (is.matrix(Q1)) Q1 <- Q1[lower.tri(Q1)]  # nolint
+  if (is.matrix(Q2)) Q2 <- Q2[lower.tri(Q2)]  # nolint
+  # capital Q is retained to conform to mathematical notation on wikipedia
+  # and in the literature
+
+  eig_q1 <- phangorn::edQt(Q1, bf) # eigen values
+  eig_q2 <- phangorn::edQt(Q2, bf) # eigen values
+
+  m <- length(levels) # always 4 (bases)
+
+  phy <- stats::reorder(phy)
+  edge <- phy$edge
+  num_nodes <- max(edge)
+
+  parent <- as.integer(edge[, 1])
+  child <- as.integer(edge[, 2])
+  root <- as.integer(parent[!match(parent, child, 0)][1])
+
+  res <- matrix(NA, l, num_nodes)
+  res[, root] <- rootseq
+  tl <- phy$edge.length
+
+  branch_subs_all <- rep(0, length(parent))
+  node_subs_all   <- rep(0, length(parent))
+
+  for (i in seq_along(tl)) {
+    from <- parent[i]
+    to <- child[i]
+
+    # first we do substitutions due to the node model:
+    P <-  P_n(100, rate2, node_time)  # get_p_matrix(tl[i], eig, rate)  # nolint
+
+    # capital P is retained to conform to mathematical notation on wikipedia
+    # and in the literature
+
+    seq_before_mut <- res[, from]
+    seq_after_mut <- t(sapply(seq_before_mut, mutate_base, P))
+    node_subs <- sum(as.numeric(seq_after_mut[, 2]))
+    res[, to] <- seq_after_mut[, 1]
+
+    # and then we add extra substitutions
+    from <- to # the parent is now the individual again
+   # P <- get_p_matrix(tl[i], eig_q1, rate1)  # nolint
+    P <-  P_n(100, rate1, tl[i])
+    # capital P is retained to conform to mathematical notation on wikipedia
+    # and in the literature
+
+    seq_before_mut <- res[, from]
+    seq_after_mut <- t(sapply(seq_before_mut, mutate_base, P))
+    branch_subs <- sum(as.numeric(seq_after_mut[, 2]))
+    res[, to] <- seq_after_mut[, 1]
+
+    branch_subs_all[i] <- branch_subs_all[i] + branch_subs
+    node_subs_all[i]   <- node_subs_all[i] + node_subs
+  }
+
+  updated_subs <- calc_accumulated_substitutions(phy, branch_subs_all,
+                                                 node_subs_all)
+
+  phy_no_extinct <- geiger::drop.extinct(phy)
+
+  k <- length(phy$tip.label)
+  label <- c(phy$tip.label, as.character((k + 1):num_nodes))
+  colnames(res) <- label
+  res <- res[, phy_no_extinct$tip.label, drop = FALSE]
+  alignment_phydat <- phyDat.DNA(as.data.frame(res, stringsAsFactors = FALSE))
+
+  total_inferred_substitutions <- sum(calc_dist(alignment_phydat, rootseq))
+
+  output <- list("alignment" = alignment_phydat,
+                 "root_seq" = rootseq,
+                 "total_branch_substitutions" = updated_subs$total_branch_subs,
+                 "total_node_substitutions" = updated_subs$total_node_subs,
+                 "total_inferred_substitutions" = total_inferred_substitutions,
+                 "total_accumulated_substitutions" =
+                   updated_subs$total_accumulated_substitutions)
+
+  return(output)
+}
