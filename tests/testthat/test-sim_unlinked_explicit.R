5f65aee4a7d0c670af7a23e8f4a38d1cf653104d
Thijs Janzen <thijsjanzen@gmail.com>
2021-09-29 12:13:35 +0200

    Update test-sim_unlinked_explicit.R

--- tests/testthat/test-sim_unlinked_explicit.R
+++ tests/testthat/test-sim_unlinked_explicit.R
@@ -3,15 +3,17 @@ context("sim_unlinked_explicit")
 test_that("sim_unlinked_explicit", {
   phy  <- phytools::read.newick(text = "(t1:10,(t3:2,t2:2):8);")
 
-  sequences <- nodeSub::sim_unlinked(phy, node_time = 0.1)
+  sequences <- nodeSub::sim_unlinked(phy, node_time = 0.1, rate1 = 0.01, rate2 = 0.01)
   testthat::expect_true(class(sequences$alignment) == "phyDat")
 
   sequences_2 <- nodeSub::sim_unlinked_explicit(phy = phy,
                                                 rootseq = sequences$root_seq,
-                                                node_time = 0.1)
+                                                node_time = 0.1,
+                                                rate1 = 0.01,
+                                                rate2 = 0.01)
 
   d1 <- calc_dist(sequences$alignment, root_sequence  = sequences$root_seq)
   d2 <- calc_dist(sequences_2$alignment, root_sequence  = sequences$root_seq)
 
-  testthat::expect_equal(sum(d1), sum(d2), tolerance = 0.05)
+  testthat::expect_equal(sum(d1), sum(d2), tolerance = 0.1)
 })
